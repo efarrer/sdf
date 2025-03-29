efarrer/spr

PR sets: track PR sets and include in spr status
PR sets will allow you to manage several sets of PRs on a single branch.
This avoids branch switching and simply groups related PRs on the same
branch. You can of course continue to work on separate branches if
desired.

This features is still a WIP.

Included in this commit:
  * Track which pull requests are a part of a pull request set (in .spr.state).
  * Include the pull request set in the spr status output.

bl/internal/state.go
@@ -7,6 +7,7 @@ import (
 	"slices"
 	"strings"
 
+	"github.com/ejoffe/spr/bl/maputils"
 	"github.com/ejoffe/spr/config"
 	"github.com/ejoffe/spr/git"
 	"github.com/ejoffe/spr/github"
@@ -32,6 +33,10 @@ type PRCommit struct {
 
 	// The index is a simple way of referring to a commit. Child commits have larger indices.
 	Index int
+
+	// The PRIndex is a simple way of referring to a set of Pull Requests. A nil PRIndex indicates that the commit doesn't
+	// have a PR (that was created by spr).
+	PRIndex *int
 }
 
 // State holds the state of the local commits and PRs
@@ -45,24 +50,29 @@ type PullRequestStatus struct {
 	Reviews        []*gogithub.PullRequestReview
 }
 
-func (prc PRCommit) String(config *config.Config) string {
-	if prc.PullRequest != nil {
-		return fmt.Sprintf("%s%2d%s %s",
-			github.ColorLightBlue,
-			prc.Index,
-			github.ColorReset,
-			prc.PullRequest.String(config),
-		)
+func indexColor(i *int) string {
+	if i == nil {
+		return github.ColorBlue
 	}
+	switch *i % 4 {
+	case 0:
+		return github.ColorRed
+	case 1:
+		return github.ColorGreen
+	case 2:
+		return github.ColorBlue
+	case 3:
+		return github.ColorLightBlue
+	}
+	return github.ColorReset
+}
+
+func (prc PRCommit) String(config *config.Config) string {
 	noPrMessage := "No Pull Request Created"
 	tempPrRemainingLen := 36
-
 	empty := github.StatusBitIcons(config)["empty"]
 
-	line := fmt.Sprintf("%s%2d%s [%s%s%s%s] %s%s : %s",
-		github.ColorLightBlue,
-		prc.Index,
-		github.ColorReset,
+	prString := fmt.Sprintf("[%s%s%s%s] %s%s : %s",
 		empty,
 		empty,
 		empty,
@@ -71,6 +81,26 @@ func (prc PRCommit) String(config *config.Config) string {
 		strings.Repeat(" ", tempPrRemainingLen),
 		prc.Commit.Subject,
 	)
+
+	if prc.PullRequest != nil {
+		prString = prc.PullRequest.String(config)
+	}
+
+	prIndex := "--"
+	if prc.PRIndex != nil {
+		prIndex = fmt.Sprintf("s%d", *prc.PRIndex)
+	}
+
+	line := fmt.Sprintf("%s%2d%s %s%s%s %s",
+		github.ColorLightBlue,
+		prc.Index,
+		github.ColorReset,
+		indexColor(prc.PRIndex),
+		prIndex,
+		github.ColorReset,
+		prString,
+	)
+
 	return github.TrimToTerminal(config, line)
 }
 
@@ -161,7 +191,7 @@ func NewState(
 ) (*State, error) {
 
 	prMap := GeneratePullRequestMap(prss)
-	gitCommits := GenerateCommits(commits)
+	gitCommits := GenerateCommits(config, commits)
 	for _, gitCommit := range gitCommits {
 		gitCommit.PullRequest = prMap[gitCommit.CommitID]
 	}
@@ -284,17 +314,30 @@ func ComputeMergeStatus(prs PullRequestStatus) github.PullRequestMergeStatus {
 	return prms
 }
 
-func GenerateCommits(commits []*object.Commit) []*PRCommit {
+func GenerateCommits(config *config.Config, commits []*object.Commit) []*PRCommit {
+	prSetMap, ok := config.State.RepoToCommitIdToPRSet[config.Repo.GitHubRepoName]
+	if !ok {
+		prSetMap = map[string]int{}
+	}
 	gitCommits := make([]*PRCommit, 0, len(commits))
 
 	// Make sure that commits are always stored HEAD first.
 	commits = HeadFirst(commits)
 
+	// Purge any mappings that aren't used
+	purgeMap := maputils.NewGC(prSetMap)
+
 	var child *PRCommit
 	for i, cm := range commits {
+		var prIndexPtr *int
+		commitId := CommitId(cm.Message)
+		if prIndex, ok := purgeMap.Lookup(commitId); ok {
+			prIndexPtr = &prIndex
+		}
+
 		c := &PRCommit{
 			Commit: git.Commit{
-				CommitID:   CommitId(cm.Message),
+				CommitID:   commitId,
 				CommitHash: cm.Hash.String(),
 				Subject:    Subject(cm.Message),
 				Body:       Body(cm.Message),
@@ -304,6 +347,7 @@ func GenerateCommits(commits []*object.Commit) []*PRCommit {
 			Parent:      nil,
 			PullRequest: nil,
 			Index:       len(commits) - (i + 1),
+			PRIndex:     prIndexPtr,
 		}
 		// Point the previous one to us
 		if child != nil {
@@ -312,6 +356,8 @@ func GenerateCommits(commits []*object.Commit) []*PRCommit {
 		gitCommits = append(gitCommits, c)
 		child = c
 	}
+	config.State.RepoToCommitIdToPRSet[config.Repo.GitHubRepoName] = purgeMap.PurgeUnaccessed()
+
 	return gitCommits
 }
 

efarrer/sdf

bl/internal/state.go
@@ -32,6 +32,10 @@ type PRCommit struct {
 
 	// The index is a simple way of referring to a commit. Child commits have larger indices.
 	Index int
+
+	// The PRIndex is a simple way of referring to a set of Pull Requests. A nil PRIndex indicates that the commit doesn't
+	// have a PR (that was created by spr).
+	PRIndex *int
 }
 
 // State holds the state of the local commits and PRs
@@ -46,24 +50,29 @@ type PullRequestStatus struct {
 	Reviews        []*gogithub.PullRequestReview
 }
 
-func (prc PRCommit) String(config *config.Config) string {
-	if prc.PullRequest != nil {
-		return fmt.Sprintf("%s%2d%s %s",
-			github.ColorLightBlue,
-			prc.Index,
-			github.ColorReset,
-			prc.PullRequest.String(config),
-		)
+func indexColor(i *int) string {
+	if i == nil {
+		return github.ColorBlue
 	}
+	switch *i % 4 {
+	case 0:
+		return github.ColorRed
+	case 1:
+		return github.ColorGreen
+	case 2:
+		return github.ColorBlue
+	case 3:
+		return github.ColorLightBlue
+	}
+	return github.ColorReset
+}
+
+func (prc PRCommit) String(config *config.Config) string {
 	noPrMessage := "No Pull Request Created"
 	tempPrRemainingLen := 36
-
 	empty := github.StatusBitIcons(config)["empty"]
 
-	line := fmt.Sprintf("%s%2d%s [%s%s%s%s] %s%s : %s",
-		github.ColorLightBlue,
-		prc.Index,
-		github.ColorReset,
+	prString := fmt.Sprintf("[%s%s%s%s] %s%s : %s",
 		empty,
 		empty,
 		empty,
@@ -72,6 +81,26 @@ func (prc PRCommit) String(config *config.Config) string {
 		strings.Repeat(" ", tempPrRemainingLen),
 		prc.Commit.Subject,
 	)
+
+	if prc.PullRequest != nil {
+		prString = prc.PullRequest.String(config)
+	}
+
+	prIndex := "--"
+	if prc.PRIndex != nil {
+		prIndex = fmt.Sprintf("s%d", *prc.PRIndex)
+	}
+
+	line := fmt.Sprintf("%s%2d%s %s%s%s %s",
+		github.ColorLightBlue,
+		prc.Index,
+		github.ColorReset,
+		indexColor(prc.PRIndex),
+		prIndex,
+		github.ColorReset,
+		prString,
+	)
+
 	return github.TrimToTerminal(config, line)
 }
 
@@ -166,6 +195,7 @@ func NewState(
 	for _, gitCommit := range gitCommits {
 		gitCommit.PullRequest = prMap[gitCommit.CommitID]
 	}
+
 	SetStackedCheck(config, gitCommits)
 
 	return &State{Commits: gitCommits}, nil
@@ -293,9 +323,11 @@ func GenerateCommits(commits []*object.Commit) []*PRCommit {
 
 	var child *PRCommit
 	for i, cm := range commits {
+		commitId := CommitId(cm.Message)
+
 		c := &PRCommit{
 			Commit: git.Commit{
-				CommitID:   CommitId(cm.Message),
+				CommitID:   commitId,
 				CommitHash: cm.Hash.String(),
 				Subject:    Subject(cm.Message),
 				Body:       Body(cm.Message),
@@ -305,6 +337,7 @@ func GenerateCommits(commits []*object.Commit) []*PRCommit {
 			Parent:      nil,
 			PullRequest: nil,
 			Index:       len(commits) - (i + 1),
+			PRIndex:     nil,
 		}
 		// Point the previous one to us
 		if child != nil {
@@ -313,6 +346,7 @@ func GenerateCommits(commits []*object.Commit) []*PRCommit {
 		gitCommits = append(gitCommits, c)
 		child = c
 	}
+
 	return gitCommits
 }
 